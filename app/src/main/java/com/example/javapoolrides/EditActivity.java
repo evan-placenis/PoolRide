8429d559d656aa98327142e83e1ca55458fab792
Evan Placenis <placenie@mcmaster.ca>
2023-04-05 11:18:50 -0400

    Initial commit

---
+++ app/src/main/java/com/example/javapoolrides/EditActivity.java
@@ -0,0 +1,15 @@
+package com.example.javapoolrides;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+        setTitle("Edit Account");
+    }
+}
---
+++ app/src/main/java/com/example/javapoolrides/RegistrationActivity.java
@@ -0,0 +1,29 @@
+package com.example.javapoolrides;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        setTitle("Registration");
+    }
+
+    public void submitForm(View v){
+        //launch a new activity
+
+        Intent i = new Intent(this, MainActivity.class);
+        //
+        String message = ((EditText)findViewById(R.id.userName)).getText().toString();
+        i.putExtra("Registation", message);
+
+        startActivity(i);
+    }
+}
