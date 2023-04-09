1e318b71d0ef5f5c23fad7907319593ce6d35507
Derek Li <derekli33@hotmail.com>
2023-04-09 17:32:33 -0400

    stored questionnaire results into variables

--- app/src/main/java/com/example/javapoolrides/RegistrationActivities/QuestionnaireActivity.java
+++ app/src/main/java/com/example/javapoolrides/RegistrationActivities/QuestionnaireActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.example.javapoolrides.CustomerHomeActivity;
 import com.example.javapoolrides.R;
@@ -19,6 +22,30 @@ public class QuestionnaireActivity extends AppCompatActivity {
     }
 
     public void submit(View v) {
+        RadioGroup ageGroup;
+        RadioGroup silentGroup;
+        RadioGroup tobaccoGroup;
+
+        RadioButton ageButton;
+        RadioButton silentButton;
+        RadioButton tobaccoButton;
+
+        ageGroup = (RadioGroup) findViewById(R.id.Age);
+        silentGroup = (RadioGroup) findViewById(R.id.Silent);
+        tobaccoGroup = (RadioGroup) findViewById(R.id.Tobacco);
+
+        int ageId = ageGroup.getCheckedRadioButtonId();
+        ageButton = (RadioButton) findViewById(ageId);
+
+        int silentId = silentGroup.getCheckedRadioButtonId();
+        silentButton = (RadioButton) findViewById(silentId);
+
+        int tobaccoId = tobaccoGroup.getCheckedRadioButtonId();
+        tobaccoButton = (RadioButton) findViewById(tobaccoId);
+
+        Toast.makeText(QuestionnaireActivity.this,
+                tobaccoButton.getText(), Toast.LENGTH_SHORT).show();
+
         Intent i = new Intent(this, CustomerHomeActivity.class);
         startActivity(i);
     }
