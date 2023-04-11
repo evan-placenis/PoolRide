170301b64b3af8f63dbf422890ff9127b5c56435
Evan Placenis <placenie@mcmaster.ca>
2023-04-10 23:29:04 -0400

    fixed error

--- app/src/main/java/com/example/javapoolrides/Databases/ScheduledRides/ScheduleDatabase.java
+++ app/src/main/java/com/example/javapoolrides/Databases/ScheduledRides/ScheduleDatabase.java
@@ -1,11 +1,12 @@
 package com.example.javapoolrides.Databases.ScheduledRides;
 
 import androidx.room.Database;
+import androidx.room.RoomDatabase;
 
 import com.example.javapoolrides.Databases.Order.Order;
 import com.example.javapoolrides.Databases.Order.OrderDao;
 
 @Database(entities = {Schedule.class},version = 1)
-public abstract class ScheduleDatabase {
+public abstract class ScheduleDatabase extends RoomDatabase {
     public abstract ScheduleDao scheduleDao();
 }
