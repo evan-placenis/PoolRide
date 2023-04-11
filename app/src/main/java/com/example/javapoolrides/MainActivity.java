2f96968a2b1aa0859f8364a6f3f7dcd0a4e9b26a
Derek Li <derekli33@hotmail.com>
2023-04-11 18:59:53 -0400

    changed name

--- app/src/main/java/com/example/javapoolrides/MainActivity.java
+++ app/src/main/java/com/example/javapoolrides/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("PoolRide");
+        setTitle("Pool Party");
 
         //Inserting Drivers into database
         DriverDatabase db = Room.databaseBuilder(getApplicationContext(),
