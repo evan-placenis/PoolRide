ec06e02aa1097aacd2f2db883b7487c4c97967b6
Derek Li <derekli33@hotmail.com>
2023-04-11 20:17:51 -0400

    did driverdestinationactivity

--- app/src/main/java/com/example/javapoolrides/DriverRideActivities/DriverDestinationActivity.java
+++ app/src/main/java/com/example/javapoolrides/DriverRideActivities/DriverDestinationActivity.java
@@ -2,8 +2,11 @@ package com.example.javapoolrides.DriverRideActivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.javapoolrides.DriverHomeActivity;
 import com.example.javapoolrides.R;
 
 public class DriverDestinationActivity extends AppCompatActivity {
@@ -13,5 +16,16 @@ public class DriverDestinationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_destination);
         setTitle("Destination");
+    }
+
+    public void more(View v) {
+        Intent i = new Intent(this, RideHomeActivity.class);
+        startActivity(i);
+    }
+
+
+    public void goHome(View v) {
+        Intent i = new Intent(this, DriverHomeActivity.class);
+        startActivity(i);
     }
 }
